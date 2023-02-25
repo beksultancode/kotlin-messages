29b21f75a18d63cae81682e7e1b3ec0bf656702d
BeksultanDev <beksultancs@gmail.com>
2023-02-25 18:39:11 +0600

    Initial commit

---
+++ src/test/kotlin/kotlinmessages/KotlinMessagesApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotlinmessages
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinMessagesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
